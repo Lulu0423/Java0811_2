75450e32381e4af3350908441290b38ae30a9ada
MB-study <MB-study@192.168.1.96>
2020-08-19 11:45:52 +0800

    Java

--- src/main/java/com/pcschool/ocp/d08/army/Car.java
+++ src/main/java/com/pcschool/ocp/d08/army/Car.java
@@ -1,13 +1,14 @@
 package com.pcschool.ocp.d08.army;
 
 public abstract class Car {
-    public Car() {   
-        System.out.println("Car...");  
-    }  
-    abstract void move(); 
+
+    public Car() {
+        System.out.println("Car...");
+    }
+
+    abstract void move();
 
     void shoot() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
-
