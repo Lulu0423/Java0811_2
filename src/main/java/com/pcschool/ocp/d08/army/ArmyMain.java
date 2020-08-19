22d4cbbee1ecab26eedbb2df57bdc21a3b0ef86b
MB-study <MB-study@192.168.1.96>
2020-08-19 15:59:19 +0800

    Java

--- src/main/java/com/pcschool/ocp/d08/army/ArmyMain.java
+++ src/main/java/com/pcschool/ocp/d08/army/ArmyMain.java
@@ -1,5 +1,7 @@
 package com.pcschool.ocp.d08.army;
 
+import java.util.Arrays;
+
 public class ArmyMain {
 
     public static void main(String[] args) {
@@ -11,18 +13,22 @@ public class ArmyMain {
             if (car instanceof Tank) {
                 ((Tank) car).shoot();
             }
+            System.out.println("-------------");
             // weapons 統一發射武器
             for (Weapon w : weapons) {
                 w.shoot();
             }
+            System.out.println("-------------");
             // cars 統一發射武器 (java8)
-            for (Car c : cars) {
-                c.shoot();
-            }
+            Arrays.stream(cars)
+                    .filter(c -> c instanceof Weapon)
+                    .map(c -> (Weapon) c)
+                    .forEach(w -> w.shoot());
+
+            System.out.println("-------------");
             // weapons 統一發射武器 (java8)
-            for (Weapon w : weapons) {
-                w.shoot();
-            }
+            Arrays.stream(weapons).forEach(w -> w.shoot());
+            
         }
     }
 }
